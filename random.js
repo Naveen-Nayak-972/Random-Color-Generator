5e1e13179ee363d37777bfa8ce00b21e0ab0f7e2
Naveen-Nayak-972 <naveennayak972@gmail.com>
2021-12-09 11:32:31 +0530

    random color generator

---
+++ random.js
@@ -0,0 +1,11 @@
+ function random(){
+    
+        const randomColor = Math.floor(Math.random()*16777215).toString(16);
+        document.body.style.backgroundColor = "#" + randomColor;
+       
+      
+      genNew.addEventListener("click", random);
+      setBg();
+ }
+
+ 
